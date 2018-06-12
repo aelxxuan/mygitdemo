16a353e8d067d4b77d47848287b7056cb8c10e2f
alexxuan <xuanzzzjjj@163.com>
2018-06-12 11:13:08 +0800

    增加usercontroller

---
+++ src/main/java/com/xuan/mysingle/console/user/UserController.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2016 the xkw.com authors.
+ * http://www.xkw.com
+ */
+package com.xuan.mysingle.console.user;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author xuanzongjun
+ * @since 1.0
+ * Date: 2018/6/12
+ */
+@Controller
+public class UserController {
+
+}
